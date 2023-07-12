27037009d4fd5ae98067c36a11b7e1440f85f2d5
Shahman Magomadov <magomadovshahman@gmail.com>
2023-07-12 22:05:25 +0300

    start project

---
+++ src/main/java/my/web/app/MainController.java
@@ -0,0 +1,13 @@
+package my.web.app;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Hello, World!";
+	}
+}
